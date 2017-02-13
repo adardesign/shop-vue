14a90c1f46b8ce3d948d347febfb6e27fe682f31
Eliazer Braun <adardesign@gmail.com>
2017-02-12 22:56:26 -0500

    Add category filter #2

--- src/components/shop/products/ProductList.vue
+++ src/components/shop/products/ProductList.vue
@@ -1,9 +1,21 @@
 <template>
   <div>
     <h3>Products</h3>
+
+    <h4>Filters:</h4>
     <div>
-      <label>Filter by price:
-          {{searchFilter}} - {{priceHigh}}
+      <label>
+        Filter by category:
+          <select v-model="categoryFilter">
+            <option value="">All</option>
+            <option v-for="option in categories" v-bind:value="option">{{option}}</option>
+          </select>
+      </label>
+    </div>
+    <div>
+      <label>
+        Filter by price:
+          {{searchFilter || priceLow}} - {{priceHigh}}
           <input type="range" step="20" v-bind:value="priceLow" v-bind:min="priceLow" v-bind:max="priceHigh"  v-model="searchFilter" placeholder="edit me">
       </label>
     </div>
@@ -27,16 +39,20 @@ import chunk from 'chunk'
 export default {
   data(){
     return {
-      searchFilter : 0
+      searchFilter : 0,
+      categoryFilter:""
     }
   },
   computed: mapState({
     //productRows: state => chunk(state.products.all, 3)
     productRows : function(state){
       //console.log(this.$data.searchFilter)
-      let products = state.products.all.filter( (e) => e.price > this.$data.searchFilter)
+      let products = state.products.all.filter((e) => this.$data.categoryFilter ? e.category == this.$data.categoryFilter : true && e.price > this.$data.searchFilter)
 
       return chunk(products, 3)
+    },
+    categories : function(state){
+      return new Set(state.products.all.map( o => o.category)).toJSON();
     },
     priceHigh:function(state){
       if(!state.products.all.length) return 0;
